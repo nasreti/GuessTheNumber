e5834ff0bde36921c1e06fa9955b9bb8dabbad50
reikocent <nasirhussein90@proton.me>
2025-04-12 11:49:30 -0700

    GuessTheNumber V1
    fun little program using random lib in python.

---
+++ GuessTheNumber.py
@@ -0,0 +1,25 @@
+#2025-04-12
+# High and Low number guessing game. between 1-100
+
+import random
+
+
+#random number gen.
+random_num = random.randint(1, 100)
+ui = 0
+guesses = 0
+while random_num != ui:
+    ui = int(input("give me your best guess: "))
+    guesses = guesses + 1 
+    if ui < random_num:
+        print("Higher!")
+        guesses = guesses + 1        
+    elif ui > random_num:
+        print("Lower!")
+        guesses = guesses + 1
+        
+
+    if ui == 6969:
+        print(f"cheat table:hehe the number is {random_num} guess it and have fun Hehe")
+
+print(f"CONGRATS U GOT IT it took you {guesses} guesses")
